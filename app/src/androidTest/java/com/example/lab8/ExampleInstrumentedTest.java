5499b62b7c206bc12c43979ab45b7af685bbee63
weiz0123 <76544381+weiz0123@users.noreply.github.com>
2024-03-08 15:27:52 -0700

    ffdsa

--- app/src/androidTest/java/com/example/lab8/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/lab8/ExampleInstrumentedTest.java
@@ -10,6 +10,8 @@ import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 /**
  * Instrumented test, which will execute on an Android device.
  *
@@ -17,11 +19,53 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+    private CustomList list;
     @Test
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("com.example.lab8", appContext.getPackageName());
     }
+
+    public CustomList MockCityList(){
+        list = new CustomList(null,new ArrayList<>());
+        return list;
+    }
+    public void addCityTest(){
+        list = MockCityList();
+        int listSize = list.getCount();
+        list.addCity(new City("Estevan", "SK"));
+        assertEquals(list.getCount(),listSize + 1);
+    }
+ /*
+
+    @Test
+    public void testHasCity() {
+        list = MockCityList();
+        City city = new City("Edmonton", "AB");
+        assertFalse(list.hasCity(city));
+        list.addCity(city);
+        assertTrue(list.hasCity(city));
+    }
+
+    @Test
+    public void testDeleteCity() {
+        list = MockCityList();
+        City city = new City("Vancouver", "BC");
+        list.addCity(city);
+        list.delete(city);
+        assertFalse(list.hasCity(city));
+    }
+
+    @Test
+    public void testGetCount() {
+        list = MockCityList();
+        assertEquals(0, list.getCount());
+        City city = new City("Vancouver", "BC");
+        list.addCity(city);
+        assertEquals(1, list.getCount());
+        list.delete(city);
+        assertEquals(0, list.getCount());
+    }*/
 
 }
